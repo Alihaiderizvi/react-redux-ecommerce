fed850105fd0ac4a4e91408cab3a6fc10c17a1d9
Ahaider60 <alihaiderizvi.you@gmail.com>
2021-07-28 09:58:35 +0500

    phase 03: navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+// Import: Internal Css
+import "./App.css";
+// Import: Internal Components
+import Nav from "./components/Nav";
 
 const App = () => {
-	return <></>;
+	return (
+		<>
+			<Nav />
+		</>
+	);
 };
 
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Nav = () => {
-	return <div></div>;
+	return (
+		<>
+			<div className='nav'>
+				<div className='containe'>
+					<div className='nav__container'>
+						<div className='nav__left'>
+							<h2>Ali Commerce</h2>
+						</div>
+						<div className='nav__right'>right</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Nav;

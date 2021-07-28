77fb5ff3209197d0fc9c02dfcce85c0e1cf23d16
Ahaider60 <alihaiderizvi.you@gmail.com>
2021-07-29 03:34:36 +0500

    phase 03: navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from "react";
 import "./App.css";
 // Import: Internal Components
 import Nav from "./components/Nav";
+import Home from "./components/Home";
+// Import Browser Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
 	return (
-		<>
+		<Router>
 			<Nav />
-		</>
+			<Route path='/' exact component={Home} />
+		</Router>
 	);
 };
 

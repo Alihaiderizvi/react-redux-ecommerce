c855151cf88a41bf1b7ae4483b7adc6dd3fd6ad8
Ahaider60 <alihaiderizvi.you@gmail.com>
2021-07-31 03:19:57 +0500

    phase 04: Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+// Import React Router
+import { Link } from "react-router-dom";
 import Header from "./Header";
 // Import For Redux Reducers
 import { useSelector } from "react-redux";
@@ -8,6 +10,33 @@ const Home = () => {
 	return (
 		<>
 			<Header />
+			<div className='container'>
+				<div className='row'>
+					{products.map((product) => (
+						<div className='col-3' key={product.id}>
+							<div className='product'>
+								<div className='product__img'>
+									<Link to={`/details/${product.id}`}>
+										{/* <img src={`/images/${product.image}`} alt='image name' /> */}
+									</Link>
+								</div>
+								<div className='product__name'>{product.name}</div>
+								<div className='row'>
+									<div className='col-6'>
+										<div className='product__price'>
+											<span className='actualPrice'></span>
+											<span className='discount'>{product.discount}%</span>
+										</div>
+									</div>
+									<div className='col-6'>
+										<div className='product__discount__price'></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</>
 	);
 };
